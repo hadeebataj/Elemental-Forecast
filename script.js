f614d1f764b167c3049d372a935974aef36afaf5
hadeebataj <61531844+hadeebataj@users.noreply.github.com>
2020-12-22 12:16:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
-  //CODE GOES HERE
   const weatherPromise = fetch(FULL_URL);
   return weatherPromise.then((response) => {
       return response.json();
@@ -31,7 +30,6 @@ getWeatherData = (city) => {
  */
 searchCity = () => {
   const city = document.getElementById('city-input').value;
-  // CODE GOES HERE
   getWeatherData(city)
   .then((response) => {
       console.log(response);
@@ -47,7 +45,6 @@ searchCity = () => {
  * HINT: make sure to console log the weatherData to see how the data looks like
  */
 showWeatherData = (weatherData) => {
-  //CODE GOES HERE
   document.getElementById("city-name").innerText = weatherData.name;
   document.getElementById("weather-type").innerText = weatherData.weather[0].main;
   document.getElementById("temp").innerText = weatherData.main.temp;
